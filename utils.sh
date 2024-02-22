29a2f2724b87307b90c63f6dabe82fa36164a83e
Massimiliano Mirelli <massimilianomirelli.mm@gmail.com>
2024-02-22 23:31:31 +0200

    Support running test until it fails by upstream issue ID

--- utils.sh
+++ utils.sh
@@ -5,16 +5,27 @@ if [[ -z $PULSAR_DEV_DIR ]]; then
     exit 1
 fi
 
+# pft_run_test_until_fails <pulsar_flaky_test_issue_id>
 function pft_run_test_until_fails(){
     local issue_id="$1"
+    mkdir --parents "${SCRIPT_DIR}/$issue_id"
     (
 	cd $PULSAR_DEV_DIR
-	issue_title_json=$(gh issue view "$issue_id" --json title)
-	test_file_name=$(jq -r '.title' <(echo $issue_title_json) | 
-			sed -e 's/Flaky-test: //' -e 's/\..*//')
-	test_name=$(jq -r '.title' <(echo $issue_title_json) | 
-			sed -e 's/Flaky-test: //' -e 's/.*\.//')
+	local issue_title_json=$(gh issue view "$issue_id" --json title)
+	local test_file_name=$(jq -r '.title' <(echo $issue_title_json) | 
+				   sed -e 's/Flaky-test: //' -e 's/\..*//')
+	local test_name=$(jq -r '.title' <(echo $issue_title_json) | 
+			      sed -e 's/Flaky-test: //' -e 's/.*\.//')
 	echo "Issue $issue_id is about $test_name in ${test_file_name}.java"
+	local full_test_name=$(find "$PULSAR_DEV_DIR" -name "${test_file_name}.java" -type f)
+	local test_root=$(echo "${full_test_name}" |
+			      sed -e "s#${PULSAR_DEV_DIR}##" -e "s#/# #g" |
+			      awk '{print $1}')
+	local count_of_failure_traces=$(ls -l "${SCRIPT_DIR}/$issue_id"/failure_trace_*.txt 2> /dev/null | wc -l)
+	if [[ -z $count_of_failure_traces ]]; then
+	    count_of_failure_traces=0
+	fi
+	ptbx_until_test_fails -Pcore-modules -pl "$test_root" -Dtest="${test_file_name}#${test_name}" | \
+	    tee "${SCRIPT_DIR}/$issue_id/failure_trace_$((count_of_failure_traces + 1)).txt"
     )
-    mkdir --parents "${SCRIPT_DIR}/$issue_id"
 }
