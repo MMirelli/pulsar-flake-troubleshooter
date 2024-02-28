1c4bea928299a2c5a7104e90475c27bcbc39616c
Massimiliano Mirelli <massimilianomirelli.mm@gmail.com>
2024-02-28 22:45:46 +0200

    Add support for integration tests to `run_flaky_until_fails_by_issue_id`

--- utils.sh
+++ utils.sh
@@ -16,7 +16,6 @@ function pft_run_flaky_until_fails_by_issue_id(){
 				   sed -e 's/Flaky-test: //' -e 's/\..*//')
 	local test_name=$(jq -r '.title' <(echo $issue_title_json) | 
 			      sed -e 's/Flaky-test: //' -e 's/.*\.//')
-	echo "Issue $issue_id is about $test_name in ${test_file_name}.java"
 	local full_test_name=$(find "$PULSAR_DEV_DIR" -name "${test_file_name}.java" -type f)
 	local test_root=$(echo "${full_test_name}" |
 			      sed -e "s#${PULSAR_DEV_DIR}##" -e "s#/# #g" |
@@ -25,7 +24,18 @@ function pft_run_flaky_until_fails_by_issue_id(){
 	if [[ -z $count_of_failure_traces ]]; then
 	    count_of_failure_traces=0
 	fi
-	ptbx_until_test_fails -Pcore-modules -pl "$test_root" -Dtest="${test_file_name}#${test_name}" | \
-	    tee "${SCRIPT_DIR}/$issue_id/failure_trace_$((count_of_failure_traces + 1)).txt"
+	{
+	    echo "Issue $issue_id is due to failure in ${test_file_name}#$test_name" 
+
+	    if [[ $test_root == tests ]]; then
+		echo "mvn install -DskipTests -Pdocker,-main"
+		mvn install -DskipTests -Pdocker,-main
+		echo "ptbx_until_test_fails -f tests/pom.xml test -DintegrationTests -Dtest=${test_file_name}#${test_name}"
+		ptbx_until_test_fails -f tests/pom.xml test -DintegrationTests -Dtest="${test_file_name}#${test_name}"
+	    else
+		echo "ptbx_until_test_fails -Pcore-modules -pl $test_root -Dtest=${test_file_name}#${test_name}"
+		ptbx_until_test_fails -Pcore-modules -pl "$test_root" -Dtest="${test_file_name}#${test_name}"
+	    fi
+	} |  tee -a "${SCRIPT_DIR}/$issue_id/failure_trace_$((count_of_failure_traces + 1)).txt"
     )
 }
